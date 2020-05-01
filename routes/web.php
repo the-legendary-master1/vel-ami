a19706763f0931a89e16efbb7081f57d378d46c3
LanzCode <lanzarroteenofre@gmail.com>
2020-05-01 15:34:53 +0800

    Starting with backend

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 
 // FrontEnd Requests
-Route::get('/', 'FrontEndController@index');
-Route::get('/product/{id}', 'FrontEndController@viewProduct')->name('product');
+	// Get Requests
+	Route::get('/', 'FrontEndController@index');
+	Route::get('/product/{id}', 'FrontEndController@viewProduct')->name('product');
 
-Route::post('/sign-up', 'FrontEndController@signUp')->name('sign-up');
+	// Post Requests
+	Route::post('/sign-up', 'FrontEndController@signUp')->name('sign-up');
 
-Auth::routes();
+	Auth::routes();
 
 // BackEnd Requests
-Route::get('/home', 'HomeController@index')->name('home');
+	// Get Requests
+	Route::get('/home', 'HomeController@index')->name('home');
